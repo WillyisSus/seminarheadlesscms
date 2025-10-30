4cb92f51987040794c73783ca91d71326c00f0f2
WillyisSus <vovietlong0845927889@gmail.com>
2025-10-30 16:37:52 +0700

    first innit for headless cms seminar

---
+++ mywebsite/mywebsite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-l#v00nm4&l@8&$xqoy4-6@0o6(x@yxkuo(5^mz1b$$bcu!x=i6"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
