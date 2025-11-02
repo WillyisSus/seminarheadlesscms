a53e650710e343523ef9c5e3316b6dadccefc07e
WillyisSus <vovietlong0845927889@gmail.com>
2025-11-02 19:29:14 +0700

    Commit another wagtail project

---
+++ mysite/mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-7bbi$-ip=!*hcsd$g)j#e%chquxjzi%w-p6so-)cx(86)193l@"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
